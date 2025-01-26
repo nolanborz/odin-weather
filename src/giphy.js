344f2eea23d08fa06e90a381816cfb6ae75f8f95
nolanborz <nolanrborzoni@gmail.com>
2025-01-26 22:48:31 +0900

    Repair giphy js

---
+++ src/giphy.js
@@ -0,0 +1,12 @@
+const searchBar = document.getElementById('searchBar');
+const giphyKey = process.env.GIPHY_API_KEY;
+
+export async function getGif(e) {
+  e.preventDefault();
+  const img = document.querySelector('img');
+  const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${giphyKey}&s=${searchBar.value}`, {mode: 'cors'})
+  .then(response => response.json()) 
+  .then(response => {
+    img.src = response.data.images.original.url;
+  });
+}
--- src/weather.js
+++ src/weather.js
@@ -1,8 +1,10 @@
+const weatherKey = process.env.WEATHER_API_KEY;
+
 export async function getWeather(location) {
   const weatherKey = process.env.WEATHER_API_KEY;
   
   return new Promise(function(resolve, reject) {
-    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${WEATHER_API_KEY}&contentType=json`)
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${weatherKey}&contentType=json`)
       .then(response => response.json())
       .then(data => reject(data.properties.peridos[1].shortForecast));
   })
