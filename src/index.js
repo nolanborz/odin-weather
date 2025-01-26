8e0dc3fa833f430ee8291cefd30d87e5c69ff436
nolanborz <nolanrborzoni@gmail.com>
2025-01-26 22:31:23 +0900

    Change giphy key

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import { getWeather } from './weather.js';
+import { displayData } from './weather.js';
 import { getGif } from './giphy.js';
 
 const form = document.getElementById('searcher-form');
 const searchBar = document.getElementById('searchBar');
-
-
-
-
-
-
-
-
+const weatherForm = document.getElementById('weather-search-form');
+const weatherSearchBar = document.getElementById('weather-search-bar');
 
 form.addEventListener('submit', getGif);
+weatherForm.addEventListener('submit', displayData());
--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,14 @@
 export async function getWeather(location) {
   const weatherKey = process.env.WEATHER_API_KEY;
+  
+  return new Promise(function(resolve, reject) {
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${WEATHER_API_KEY}&contentType=json`)
+      .then(response => response.json())
+      .then(data => reject(data.properties.peridos[1].shortForecast));
+  })
+}
+
+export async function displayData(weather) {
+   await console.log(weather);
 }
 
